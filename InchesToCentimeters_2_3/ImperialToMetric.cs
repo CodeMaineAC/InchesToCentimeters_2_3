232fc5b415a13e130e0f9b7759abf30ef691d114
Benjamin Goff <bgoff@mainerobotics.org>
2021-01-22 10:09:50 -0500

    Add project files.

---
+++ InchesToCentimeters_2_3/ImperialToMetric.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InchesToCentimeters_2_3
+{
+    class ImperialToMetric
+    {
+        const double CENTIMETERS_PER_INCH = 2.54;
+        const double KM_PER_MILE = 1.60934;
+        public static double InchesToCentimeters(double inches)
+        {
+            return inches * CENTIMETERS_PER_INCH;
+        }
+
+        public static double MilesToKilometers(double miles)
+        {
+            return miles * KM_PER_MILE;
+        }
+    }
+}
---
+++ InchesToCentimeters_2_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InchesToCentimeters_2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            double inches;
+
+            Console.WriteLine("Distance in inches: ");
+            input = Console.ReadLine();
+            inches = Convert.ToDouble(input);
+            Console.WriteLine("{0} inches is {1} centimeters", inches, ImperialToMetric.InchesToCentimeters(inches));
+        }
+    }
+}
